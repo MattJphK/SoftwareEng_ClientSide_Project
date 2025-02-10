67a24ede4d0bef298d5e293116c5765d72bd8e9b
SKolev06 <B00159340@mytudublin.ie>
2025-02-10 11:01:37 +0000

    INDEX COMMIT

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Website</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<h1>The DirctorsCut</h1>
+<h2>Best Projects </h2>
+
+<div class="product-container">
+
+</div>
+
+</body>
+</html>
+
