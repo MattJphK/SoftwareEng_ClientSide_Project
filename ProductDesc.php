3e9b4f1d1af0c312d066678f67cbeff0347d2af5
MattJphK <b00160049@mytudublin.ie>
2025-02-11 17:10:19 +0000

    Update ProductDesc.php

--- ProductDesc.php
+++ ProductDesc.php
@@ -1,20 +1,18 @@
+<?php
+$pgHeader = "Movie Descriptions";
+$movieTitle1 = "Reservoir Dogs";
+$desc1 = "One of Quentin Tarantino Best Films about a heist gone wrong. Creates a tense atmosphere
+                and enjoyable violance?";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Product Description</title>
-    <h1>?php echo Good Movies?</h1>
+    <h1><?php echo $pgHeader; ?></h1>
 </head>
 <body>
-<?php
-    $movieTitle1 = "Reservoir Dogs";
-    $desc1 = "One of Quentin Tarantino Best Films about a heist gone wrong. Creates a tense atmosphere
-                and enjoyable violance?";
-    ?>
-
-
-
-
 <h2><?php echo $movieTitle1; ?> </h2>
 <img src="images/rDogsImg.jpg" alt="Reservoir Dogs" width="300" height="300">
 <p><?php echo $desc1; ?> </p>
