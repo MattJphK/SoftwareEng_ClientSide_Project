9919edfad9c2cd6f2f0d9fbdd62e34b6ef1663a2
MattJphK <b00160049@mytudublin.ie>
2025-02-21 23:37:56 +0000

    Create Review.php

---
+++ Review.php
@@ -0,0 +1,56 @@
+<?php
+$pgHeader = "Movie Descriptions";
+$movieTitle1 = "Movie Title 1";
+$desc1 = "One of Quentin Tarantino Best Films about a heist gone wrong. Creates a tense atmosphere
+                and enjoyable violance?" . "One of Quentin Tarantino Best Films about a heist gone wrong. Creates a tense atmosphere
+                and enjoyable violance?" . "One of Quentin Tarantino Best Films about a heist gone wrong. Creates a tense atmosphere
+                and enjoyable violance?" . "One of Quentin Tarantino Best Films about a heist gone wrong. Creates a tense atmosphere
+                and enjoyable violance?";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Movie Descriptions</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/Index.css">
+    <link rel="stylesheet" href="css/Nav.css">
+</head>
+<header>
+
+</header>
+<body>
+<div class="top">
+    <div class="logo">
+        <a href="index.php" id="leftSitting"><img class="logo_img" src="images/logo.png" alt="logo" ></a>
+    </div>
+    <ul>
+
+        <li><a href="index.php">Login</a></li>
+        <li><a href="index.php">Register</a></li>
+        <li> <form class="search-form">
+                <input type="text" placeholder="Search..." class="search-input">
+                <button type="submit" class="search-btn"><img src="images/icons/loupe.png"></button>
+            </form>
+        </li>
+    </ul>
+</div>
+<div class="nav">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="ProductDesc.php">Products</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+</div>
+<div class="product-container">
+    <div class="movie">
+        <form action="/action_page.php">
+            <p><label for="w3review">Review of W3Schools:</label></p>
+            <textarea id="w3review" name="w3review" rows="4" cols="50">At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea>
+            <br>
+            <input type="submit" value="Submit">
+        </form>
+        <button type="button">Book</button>
+        <button type="button">Review</button>
+    </div>
