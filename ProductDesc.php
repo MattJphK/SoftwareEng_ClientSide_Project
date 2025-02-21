20fe69495c68bcf7045d9b9aa82edef080b5ea2b
MattJphK <b00160049@mytudublin.ie>
2025-02-21 17:11:49 +0000

    Update ProductDesc.php

--- ProductDesc.php
+++ ProductDesc.php
@@ -13,24 +13,30 @@ $desc1 = "One of Quentin Tarantino Best Films about a heist gone wrong. Creates 
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="css/Index.css">
     <link rel="stylesheet" href="css/Nav.css">
-    <div class="top">
-        <div class="logo">
-            <a href="index.php" id="leftSitting"><img class="logo_img" src="images/logo.png" alt="logo" ></a>
-        </div>
-        <ul>
 </head>
 <header>
-    <div class="nav">
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="ProductDesc.php">Products</a></li>
-            <li><a href="">Contact</a></li>
-        </ul>
-    </div>
+
 </header>
 <body>
+<div class="top">
+    <div class="logo">
+        <a href="index.php" id="leftSitting"><img class="logo_img" src="images/logo.png" alt="logo" ></a>
+    </div>
+    <ul>
 
-<img src="images/movieCovers/rDogsImg.jpg" alt="Reservoir Dogs" width="300" height="300">
-<p><?php echo $desc1; ?> </p>
-</body>
-</html>
+        <li><a href="index.php">Login</a></li>
+        <li><a href="index.php">Register</a></li>
+        <li> <form class="search-form">
+                <input type="text" placeholder="Search..." class="search-input">
+                <button type="submit" class="search-btn"><img src="images/icons/loupe.png"></button>
+            </form>
+        </li>
+    </ul>
+</div>
+<div class="nav">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="ProductDesc.php">Products</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+</div>
