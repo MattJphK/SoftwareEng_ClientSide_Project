bdd9bd286520a3860efadcd8c12b11a970383cf7
MattJphK <b00160049@mytudublin.ie>
2025-02-24 10:40:35 +0000

    Update dataconn.php

--- dataconn.php
+++ dataconn.php
@@ -1,7 +1,7 @@
 <?php
 $servername = 'localhost';
 $username = 'root';
-$password = '';
+$password = 'Ilovetea24!';
 $dbname = 'movieweb';
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
