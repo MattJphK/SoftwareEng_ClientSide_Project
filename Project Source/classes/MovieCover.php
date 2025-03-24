c59341a291fb488ff756fc48985510ce59c27b30
Stan <B00159340@mytudublin.ie>
2025-03-24 01:43:55 +0000

    s

--- Project Source/classes/MovieCover.php
+++ Project Source/classes/MovieCover.php
@@ -2,20 +2,21 @@
 // MovieCover.php
 
 require_once 'movieClass.php';
-include '../src/DBconnect.php';
 
 class MovieCover extends movieClass {
     private $coverImage;
 
     // Constructor that initializes the parent class and the cover image
     public function __construct($movieid, $title, $genre, $ticket_price, $coverImage) {
+        // Call the parent constructor
         parent::__construct($movieid, $title, $genre, $ticket_price);
         $this->coverImage = $coverImage;
     }
 
     // Getter for cover image filename (returns full path)
     public function getCoverImage() {
-        return $this->coverImage;
+        // Return the path to the movie cover image inside the 'moviecovers' directory
+        return '../images/moviecovers/' . $this->coverImage;
     }
 
     // Method to set the cover image filename
@@ -24,18 +25,19 @@ class MovieCover extends movieClass {
     }
 
     // Static method to fetch all movies with their cover images from the database
-    public static function fetchAllMoviesWithCover($Connection) {
+    public static function fetchAllMoviesWithCover($dbConnection) {
         $moviesWithCover = [];
         $query = "SELECT * FROM movies";  // Query to fetch all movies
 
         // Prepare the query using PDO
-        $stmt = $Connection->prepare($query);
+        $stmt = $dbConnection->prepare($query);
         $stmt->execute();
 
         // Check if any movies were found
         if ($stmt->rowCount() > 0) {
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 // Assume the cover image filename is stored locally using the movie title
+                // Replace spaces with underscores, make the title lowercase, and remove invalid characters
                 $coverImage = strtolower(str_replace(' ', '_', $row['title']));
                 $coverImage = preg_replace('/[^a-z0-9_]/', '', $coverImage);  // Remove invalid characters
                 $coverImage .= '.jpg';  // Add .jpg extension
