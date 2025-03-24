dc328906f63735de0a6d85118e9cea8b106034fc
SKolev06 <B00159340@mytudublin.ie>
2025-03-24 10:04:31 +0000

    sd
    sd

--- Project Source/classes/MovieCover.php
+++ Project Source/classes/MovieCover.php
@@ -13,7 +13,6 @@ class MovieCover extends movieClass {
     }
 
     public function getCoverImage() {
-        // Return the path to the movie cover image inside the 'moviecovers' directory
         return '../images/moviecovers/' . $this->coverImage;
     }
 
@@ -25,18 +24,14 @@ class MovieCover extends movieClass {
         $moviesWithCover = [];
         $query = "SELECT * FROM movies";
 
-        // Prepare the query using PDO
         $stmt =  $connection->prepare($query);
         $stmt->execute();
 
-        // Check if any movies were found
         if ($stmt->rowCount() > 0) {
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 $coverImage = strtolower(str_replace(' ', '_', $row['title']));
                 $coverImage = preg_replace('/[^a-z0-9_]/', '', $coverImage);  // Remove invalid characters
-                $coverImage .= '.jpg';  // Add .jpg extension
-
-                // Create new MovieCover object with the cover image
+                $coverImage .= '.jpg';
                 $movieCover = new MovieCover(
                     $row['movieid'],
                     $row['title'],
@@ -51,7 +46,6 @@ class MovieCover extends movieClass {
 
         return $moviesWithCover;
     }
-    // Static method to fetch cover image for a specific movie by ID
     public static function fetchCoverByMovieId($connection, $movie_id) {
         $query = "SELECT * FROM movies WHERE movieid = :movieid";
 
@@ -59,18 +53,14 @@ class MovieCover extends movieClass {
         $stmt->bindParam(':movieid', $movie_id, PDO::PARAM_INT);
         $stmt->execute();
 
-        // Fetch the movie details
         $movie = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        // Check if the movie exists
         if (!$movie) {
-            return null;  // If no movie found, return null
+            return null;
         }
 
-        // Generate the cover image filename based on the movie title
         $coverImage = strtolower(str_replace([' ', ':'], ['_', ''], $movie['title'])) . '.jpg';
 
-        // Return a MovieCover object with the cover image
         return new MovieCover(
             $movie['movieid'],
             $movie['title'],
