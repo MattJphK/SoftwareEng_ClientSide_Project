f0b70491a395e70c0c3fb8b69b70ac2c203f848f
MattJphK <b00160049@mytudublin.ie>
2025-04-27 13:37:13 +0100

    ca 4 doc pdf

--- Project Source/public/admin.php
+++ Project Source/public/admin.php
@@ -4,6 +4,7 @@ include "template/header.php";
 include "template/adminNav.php";
 ?>
 
-<h1>Hello Admin</h1>
+<h1>Hello Admin:
+    Click The Logo to Return to The Non Admin Main Page</h1>
 
 <?php include "template/footer.php" ?>
